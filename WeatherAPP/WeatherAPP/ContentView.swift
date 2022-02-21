00e1728d90aff9a0b79e44cd79f08d77a9e71a03
DumitruBogdan <dbogdan999@yahoo.com>
2022-02-21 13:48:21 +0200

    Initial setup

---
+++ WeatherAPP/WeatherAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAPP
+//
+//  Created by Freak on 21.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAPP/WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Freak on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
